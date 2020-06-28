891d31a1ade9a2fc4b9fbb236acbfc7e57f26b55
Carlo Zablan <zablanc91@hotmail.com>
2020-06-27 17:54:09 -0700

    adjustments for production release

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,6 @@
 //set @ Heroku
 module.exports = {
     tokenSecret: process.env.TOKEN_SECRET,
-    sendGridKey: process.env.SENDGRID_API
+    sendGridKey: process.env.SENDGRID_API,
+    mongooseConnect: process.env.MONGOOSE_CONNECT
 };
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
+const { mongooseConnect } = require('../config/keys');
 
-//connecting to MongoDB - 127 IP is localhost (this is for local dev environment)
-if(process.env.NODE_ENV === 'dev'){
-    mongoose.connect('mongodb://127.0.0.1:27017/TaskManagerAPI', {
+
+
+//connecting to MongoDB - localhost for dev, mongoDB cluster for prod
+mongoose.connect( mongooseConnect , {
     useNewUrlParser: true,
     useCreateIndex: true
     })
     .then(() => console.log('Connected to MongoDB with mongoose.'))
     .catch(error => console.log('Error:', error));
-}
-
-
-//TODO: make an else statement to connect to MongoDB when in production environment with Heroku
